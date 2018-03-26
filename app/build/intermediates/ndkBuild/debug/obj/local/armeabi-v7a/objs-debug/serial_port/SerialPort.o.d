6107ed2c74e635c237f444a82f38a7032fec6472
Young <zhipengzhong@live.com>
2018-03-26 11:37:13 +0800

    Initial commit

---
+++ app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/serial_port/SerialPort.o.d
@@ -0,0 +1,5 @@
+D:/AndroidStudioProjects/ComAssistant/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/serial_port/SerialPort.o: \
+  D:/AndroidStudioProjects/ComAssistant/app/src/main/jni/SerialPort.c \
+  D:/AndroidStudioProjects/ComAssistant/app/src/main/jni/SerialPort.h
+
+D:/AndroidStudioProjects/ComAssistant/app/src/main/jni/SerialPort.h:
